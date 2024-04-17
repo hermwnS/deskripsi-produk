054bdd00e993e0477049f61790c786ba61bb6bea
Hermawan Saputra <126874286+hermwnS@users.noreply.github.com>
2024-04-17 17:42:24 +0700

    membuat tampilan feedback pelanggan

--- resources/views/deskripsi.blade.php
+++ resources/views/deskripsi.blade.php
@@ -38,7 +38,7 @@
                     <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
                         <li class="nav-item">
                             <a class="nav-link active" aria-current="page" href="#"><i id="keranjang" class="bi bi-cart text-black me-2" style="font-size: 25px"></i></a>
-                        </li>
+                        </li> 
 
                         <li class="nav-item">
                             <a class="nav-link active" aria-current="page" href="#"><i class="bi bi-person-circle text-black me-1" style="font-size: 25px"></i></a>
